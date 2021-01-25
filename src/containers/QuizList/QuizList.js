840883309c48e021067f9b038e28eed044d1e2cf
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-01-25 21:44:02 +0300

    Страница списка тестов

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import classes from './QuizList.module.css'
+import { NavLink } from 'react-router-dom'
 
 class QuizList extends Component {
 
+   renderQuizzes() {
+      return [1, 2, 3].map((quiz, index) => {
+         return (
+            <li
+               key={index}
+            >
+               <NavLink to={ 'quiz/' + quiz }>
+                  Тест {quiz}
+               </NavLink>
+            </li>
+         )
+      })
+   }
+
    render() {
       return (
-         <div>
-            <h1>QuizList</h1>
+         <div className={ classes.QuizList }>
+            <div>
+               <h1>Список тестов</h1>
+               <ul>
+                  { this.renderQuizzes() }
+               </ul>
+            </div>
          </div>
       )
    }
