9c41b24f3bcc1c979505874823576f79080be251
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-01-25 21:30:56 +0300

    Навигация

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 import classes from './Drawer.module.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
+import { NavLink } from 'react-router-dom'
 
-const links = [1, 2, 3]
+const links = [
+   { to: '/', label: 'Список тестов', exact: true },
+   { to: '/auth', label: 'Авторизация', exact: false },
+   { to: '/quiz-creator', label: 'Создать тест', exact: false }
+]
 
 class Drawer extends Component {
+
+   clickHandler = () => {
+      this.props.onClose()
+   }
 
    renderLinks() {
       return links.map((link, index) => {
          return (
             <li key={ index }>
-               <a>Link { link }</a>
+               <NavLink
+                  to={ link.to }
+                  exact={ link.exact }
+                  activeClassName={ classes.active }
+                  onClick={this.clickHandler}
+               >
+                  { link.label }
+               </NavLink>
             </li>
          )
       })
@@ -30,7 +46,7 @@ class Drawer extends Component {
                   { this.renderLinks() }
                </ul>
             </nav>
-            { this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+            { this.props.isOpen ? <Backdrop onClick={ this.props.onClose } /> : null }
          </React.Fragment>
       )
    }
---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class QuizList extends Component {
+
+   render() {
+      return (
+         <div>
+            <h1>QuizList</h1>
+         </div>
+      )
+   }
+}
+
+export default QuizList
