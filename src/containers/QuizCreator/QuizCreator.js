6223f1ca44f94febdc1ef96527c0cf7e70996f63
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-01-27 22:03:00 +0300

    Создание компонента Select

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
-import { createControl } from '../../form/formFramework'
 import Input from '../../components/UI/Input/Input'
+import Select from '../../components/UI/Select/Select'
+import { createControl } from '../../form/formFramework'
 
 function createOptionControl(number) {
    return createControl({
@@ -28,6 +29,7 @@ class QuizCreator extends Component {
 
    state = {
       quiz: [],
+      rightAnswerId: 1,
       formControls: createFormControls()
    }
 
@@ -68,7 +70,25 @@ class QuizCreator extends Component {
       })
    }
 
+   selectChangeHandler = event => {
+      this.setState({
+         rightAnswerId: +event.target.value
+      })
+   }
+
    render() {
+      const select = <Select
+         label='Выберите правильный ответ'
+         value={ this.state.rightAnswerId }
+         onChange={ this.selectChangeHandler }
+         options={ [
+            { text: 1, value: 1 },
+            { text: 2, value: 2 },
+            { text: 3, value: 3 },
+            { text: 4, value: 4 }
+         ] }
+      />
+
       return (
          <div className={ classes.QuizCreator }>
             <div>
@@ -78,7 +98,7 @@ class QuizCreator extends Component {
 
                   { this.renderControls() }
 
-                  <select></select>
+                  { select }
 
                   <Button
                      type='primary'
